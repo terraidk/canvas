207c7be8d3b68b290b7a2920ba464c4334b9a1c3
terraidk <133658501+terraidk@users.noreply.github.com>
2023-05-26 13:02:59 +0200

    Add files via upload

---
+++ phpweek3.php
@@ -0,0 +1,49 @@
+<?php
+// Deel 1
+// $myArray = ['auto', 'fiets', 'brommer', 'bus', 'vliegtuig', 'trein'];
+// $myArray[] = 'boot';
+
+// foreach ($myArray as $value) {
+//    echo $value . "<br>";
+//}
+
+
+// Deel 2
+
+// $myArray = ['auto', 'fiets', 'brommer', 'bus', 'vliegtuig', 'trein'];
+// $elements = count($myArray);
+
+// echo "Het array heeft $elements elementen.";
+
+
+// Deel 3
+
+// $array = array("foo","bar","hello","world");
+// echo $array[1]; // uitvoer = bar
+
+// $array = array(1,2,3,4,5);
+// echo $array[3]; // uitvoer = "4"
+
+// $cars = array("Volvo", "BMW", "Toyota");
+// echo $array[2]; // uitvoer = Toyota
+// echo count($cars); // uitvoer = 3
+
+// $array = [ 5, 4, 3, 2, 1 ];
+// echo $array[0]; // uitvoer = 5
+
+// $numbers[0]="one";
+// $numbers[1]="two";
+// $numbers[2]="three";
+// $numbers[3]="four";
+// $numbers[4]="five";
+// echo $numbers[count($numbers)-1]; // uitvoer = five
+
+
+// Deel 4
+
+// $cijfersPHP = [4.4, 4.6, 5.6, 6.1, 7.6, 7.2];
+// $gemiddelde = round(array_sum($cijfersPHP) / count($cijfersPHP), 1);
+// echo "Gemiddelde is: ".$gemiddelde;
+
+?>
+

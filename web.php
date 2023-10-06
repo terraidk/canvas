572ccc0ff8ca1c51e074f85126759109c5fe6f98
terraidk <133658501+terraidk@users.noreply.github.com>
2023-10-06 16:14:47 +0200

    Update web.php

--- web.php
+++ web.php
@@ -13,9 +13,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-$planets = ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
- 
 
-dd($planets);
+Route::get('/planets', function () {
+    $name = request('name');
+    $planets = [
+        [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
+        ],
+        [
+            'name' => 'Jupiter',
+            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+        ],
+    ];
+
+    if ($name) {
+        $filteredPlanets = array_filter($planets, function ($planet) use ($name) {
+            return strcasecmp($planet['name'], $name) === 0;
+        });
+
+        $planets = $filteredPlanets;
+    }
+
+    return view('planets')->with(['planeten' => $planets]);
 });
